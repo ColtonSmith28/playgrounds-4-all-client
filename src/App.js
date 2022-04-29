ce63c7d668b04baf7342e5d26a9eff34146e87e8
ColtonSmith28 <coltonsmith28@gmail.com>
2022-04-28 19:42:05 -0500

    Added imports for map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.scss'
+import './App.scss';
+import Map from './components/Map/Map.js';
 
 function App() {
   return (
     <>
       <h1> Hello Nicholas & Colton</h1>
+      <Map />
     </>
   );
 }
